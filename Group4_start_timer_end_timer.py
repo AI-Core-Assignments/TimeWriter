a015e5c455f034bf80d8d5451374733806cace85
Tanya Zakharchenko <t.a.zakharchenko@gmail.com>
2020-08-01 14:11:22 +0100

    Group 4 task complete

---
+++ Group4_start_timer_end_timer.py
@@ -0,0 +1,37 @@
+import datetime
+from datetime import timedelta
+import time
+
+# %%
+class Timer():
+    def __init__(self, start_time = None, time_ellapsed = None):
+        self.start_time = start_time
+        self.time_ellapsed = time_ellapsed
+        
+    def start_timer(self):
+        self.start_time = datetime.datetime.now() # getting the start timepoint
+        print('Start time: ',  self.start_time)
+        return self.start_time
+    
+    def end_timer(self):
+        self.time_ellapsed =  datetime.datetime.now() # getting the end timepoint
+        print('End time: ',  self.time_ellapsed)
+        return self.time_ellapsed
+    
+    def formatting_seconds(self):
+        time_difference = timedelta.total_seconds(self.time_ellapsed - self.start_time)
+        print(time_difference)
+    
+timer = Timer()
+timer.start_timer()
+timer.end_timer()
+timer.formatting_seconds()
+
+# start_time = timer.start_timer()
+# time.sleep(2)
+# end_time = timer.end_timer()
+# print(end_time - start_time)
+
+
+
+
